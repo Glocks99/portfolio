e4e0d4760a7bdcb80940d06563ebf93f0795a640
James <jameslarbie30@gmail.com>
2025-01-27 23:30:39 +0000

    mobile  view done

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+
+    document.querySelector("nav .fa-bars").addEventListener("click", e => {
+        document.querySelector("nav").classList.add("active")
+    })
+
+    document.querySelector(".menu .close").addEventListener("click", e => {
+        document.querySelector("nav").classList.remove("active")
+    })
+
+    document.querySelectorAll("#recents button").forEach(btn => {
+        btn.addEventListener("click", e => {
+            document.querySelector(".modals").style = "display: initial"
+        })
+    })
+
+    document.querySelector(".cont .fa-x").addEventListener("click", e => {
+        document.querySelector(".modals").style = "display: none"
+    })
+
+    document.querySelectorAll(".menu ul li").forEach(link => {
+        link.addEventListener("click", e => {
+            document.querySelector("nav").classList.remove("active")
+        })
+    })
+
+    document.querySelector("#hero button").addEventListener("click", e => {
+        window.location.href = "#recents"
+    })
+})
